82354082e87d9a01b8557ba07939af543504b9f2
baba-s <shotababa1206@gmail.com>
2022-10-06 21:05:53 +0900

    update

--- Runtime/GetComponentCache.cs
+++ Runtime/GetComponentCache.cs
@@ -5,10 +5,15 @@ namespace Kogane
     /// <summary>
     /// GetComponent の結果をキャッシュしておくためのクラス
     /// </summary>
-    public sealed class ComponentCache<T>
+    public sealed class GetComponentCache<T>
     {
         private T    m_cache;
         private bool m_isCached;
+
+        public T Get( Component component )
+        {
+            return Get( component.gameObject );
+        }
 
         public T Get( GameObject gameObject )
         {
---
+++ Runtime/GetOrAddComponentCache.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Kogane
+{
+    /// <summary>
+    /// GetComponent か AddComponent の結果をキャッシュしておくためのクラス
+    /// </summary>
+    public sealed class GetOrAddComponentCache<T> where T : Component
+    {
+        private T    m_cache;
+        private bool m_isCached;
+
+        public T Get( Component component )
+        {
+            return Get( component.gameObject );
+        }
+
+        public T Get( GameObject gameObject )
+        {
+            // UnityEngine.Object の null チェックは少し負荷がかかるため
+            // bool 値で判定するようにしています
+            if ( m_isCached ) return m_cache;
+            if ( m_cache != null ) return m_cache;
+
+            m_cache = gameObject.GetComponent<T>();
+
+            if ( m_cache == null )
+            {
+                m_cache = gameObject.AddComponent<T>();
+            }
+
+            m_isCached = m_cache != null;
+
+            return m_cache;
+        }
+    }
+}
