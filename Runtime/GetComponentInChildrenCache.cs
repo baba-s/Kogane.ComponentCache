b5a2b2103967ef7a202ca0d48bcad413869a5626
baba-s <shotababa1206@gmail.com>
2022-11-06 08:07:17 +0900

    add

--- Runtime/GetComponentInChildrenCache.cs
+++ Runtime/GetComponentInChildrenCache.cs
@@ -3,26 +3,26 @@
 namespace Kogane
 {
     /// <summary>
-    /// GetComponent の結果をキャッシュしておくためのクラス
+    /// GetComponentInChildren の結果をキャッシュしておくためのクラス
     /// </summary>
-    public sealed class GetComponentCache<T>
+    public sealed class GetComponentInChildrenCache<T>
     {
         private T    m_cache;
         private bool m_isCached;
 
-        public T Get<TComponent>( TComponent component ) where TComponent : Component
+        public T Get<TComponent>( TComponent component, bool includeInactive = true ) where TComponent : Component
         {
-            return Get( component.gameObject );
+            return Get( component.gameObject, includeInactive );
         }
 
-        public T Get( GameObject gameObject )
+        public T Get( GameObject gameObject, bool includeInactive = true )
         {
             // UnityEngine.Object の null チェックは少し負荷がかかるため
             // bool 値で判定するようにしています
             if ( m_isCached ) return m_cache;
             if ( m_cache != null ) return m_cache;
 
-            m_cache    = gameObject.GetComponent<T>();
+            m_cache    = gameObject.GetComponentInChildren<T>( includeInactive );
             m_isCached = m_cache != null;
 
             return m_cache;
--- Runtime/GetComponentInChildrenOrAddCache.cs
+++ Runtime/GetComponentInChildrenOrAddCache.cs
@@ -3,26 +3,26 @@
 namespace Kogane
 {
     /// <summary>
-    /// GetComponent か AddComponent の結果をキャッシュしておくためのクラス
+    /// GetComponentInChildren の結果をキャッシュしておくためのクラス
     /// </summary>
-    public sealed class GetOrAddComponentCache<T> where T : Component
+    public sealed class GetComponentInChildrenOrAddCache<T> where T : Component
     {
         private T    m_cache;
         private bool m_isCached;
 
-        public T Get<TComponent>( TComponent component ) where TComponent : Component
+        public T Get<TComponent>( TComponent component, bool includeInactive = true ) where TComponent : Component
         {
-            return Get( component.gameObject );
+            return Get( component.gameObject, includeInactive );
         }
 
-        public T Get( GameObject gameObject )
+        public T Get( GameObject gameObject, bool includeInactive = true )
         {
             // UnityEngine.Object の null チェックは少し負荷がかかるため
             // bool 値で判定するようにしています
             if ( m_isCached ) return m_cache;
             if ( m_cache != null ) return m_cache;
 
-            m_cache = gameObject.GetComponent<T>();
+            m_cache = gameObject.GetComponentInChildren<T>( includeInactive );
 
             if ( m_cache == null )
             {
